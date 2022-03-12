550c814e560a51188207a6c597659bdf2999d5ac
David Grudl <david@grudl.com>
2022-03-12 05:02:39 +0100

    CompileException: line & column are passed to constructor

--- src/Latte/Compiler/Tag.php
+++ src/Latte/Compiler/Tag.php
@@ -120,17 +120,17 @@ final class Tag
 	public function validate(string|bool|null $arguments, array $parents = [], bool $modifiers = false): void
 	{
 		if ($parents && (!$this->parentNode || !in_array($this->parentNode->name, $parents, true))) {
-			throw new CompileException('Tag ' . $this->getNotation() . ' is unexpected here.');
+			throw new CompileException('Tag ' . $this->getNotation() . ' is unexpected here.', $this->startLine);
 
 		} elseif ($this->modifiers !== '' && !$modifiers) {
-			throw new CompileException('Filters are not allowed in ' . $this->getNotation());
+			throw new CompileException('Filters are not allowed in ' . $this->getNotation(), $this->startLine);
 
 		} elseif ($arguments && $this->args === '') {
 			$label = is_string($arguments) ? $arguments : 'arguments';
-			throw new CompileException('Missing ' . $label . ' in ' . $this->getNotation());
+			throw new CompileException('Missing ' . $label . ' in ' . $this->getNotation(), $this->startLine);
 
 		} elseif ($arguments === false && $this->args !== '') {
-			throw new CompileException('Arguments are not allowed in ' . $this->getNotation());
+			throw new CompileException('Arguments are not allowed in ' . $this->getNotation(), $this->startLine);
 		}
 	}
 }
